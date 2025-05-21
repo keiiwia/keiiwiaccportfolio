22fa6fe174b7a867d5f493cbd9f9e55ad9112056
keiiwia <kellysu.usa@gmail.com>
2025-05-20 21:54:34 -0700

    fix?

--- advcc/experimentalcam/sketch.js
+++ advcc/experimentalcam/sketch.js
@@ -16,11 +16,11 @@ let eightiesbutton, ninetiesbutton, tensbutton, funbutton;
 
 //preload all assets (camera/filters)
 function preload() {
-  img = loadImage('assets/vhsbackground.jpg');
-  supereight = loadImage('assets/supereightfilter.png');
-  kodak = loadImage('assets/kodakoverlay.jpg');
-  polaroid = loadImage('assets/polaroid.png');
-  filmburn = loadImage('assets/filmburn.jpg');
+  img = loadImage('./assets/vhsbackground.jpg');
+  supereight = loadImage('./assets/supereightfilter.png');
+  kodak = loadImage('./assets/kodakoverlay.jpg');
+  polaroid = loadImage('./assets/polaroid.png');
+  filmburn = loadImage('./assets/filmburn.jpg');
   }
 
 function setup() {
